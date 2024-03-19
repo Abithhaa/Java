6a0011e4c15a5e8cdeb837213646d83b036a8ef6
Abithhaa <159014443+Abithhaa@users.noreply.github.com>
2024-03-19 15:49:03 +0530

    Create replace.java

---
+++ replace.java
@@ -0,0 +1,8 @@
+public class ReplaceString {
+    public static void main(String[] args) {
+        String s1 = "Today is Monday";
+        System.out.println("String is: " + s1);
+        String s2 = s1.replace("is", "=");
+        System.out.println(s2);
+    }
+}
